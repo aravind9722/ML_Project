03c9be3e0835dfc8f4f0fe93783f784e66ce3097
Aravind <aravind@ineuron.ai>
2022-07-18 16:34:13 +0530

    V 1.1

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from typing import List
 
 
@@ -8,10 +8,9 @@ PROJECT_NAME= "housing-price-predictor"
 VERSION= "0.0.1"
 AUTHOR= "Aravind"
 DESCRIPTION="US-Housing Price predictor ML project"
-PACKAGES=["housing"]
-REQUIREMENT_FILE_NAME="requiremnts.txt"
+REQUIREMENT_FILE_NAME="requirements.txt"
 
-def get_requirements_list()->List(str):
+def get_requirements_list()->List[str]:
     """
     Description: Function is going to return list of requirement
     mentioned in requirements.txt file
@@ -20,7 +19,7 @@ def get_requirements_list()->List(str):
     mentioned in requirements.txt file
     """
     with open(REQUIREMENT_FILE_NAME)as requirement_file:
-        return requirement_file.readlines()
+        return requirement_file.readlines().remove("-e .")
 
 
 setup(
@@ -28,9 +27,6 @@ name= PROJECT_NAME,
 version=VERSION,
 author=AUTHOR,
 description=DESCRIPTION,
-packages=PACKAGES,
+packages=find_packages(),
 install_requires=get_requirements_list()
 )
-
-if __name__=="__main__":
-    print(get_requirements_list())
